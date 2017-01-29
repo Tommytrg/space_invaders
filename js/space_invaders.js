d1ec3ade64ae1d5bc8b3cb6e14003af82ddc432e
Tomas Ruiz <tomastrg22@gmail.com>
2017-01-29 18:35:46 +0100

    first alien and shooting functions created

--- js/space_invaders.js
+++ js/space_invaders.js
@@ -28,36 +28,69 @@ Ship.prototype.move = function(direction){
 };
 
 Ship.prototype.moveRight = function(){
-  this.position.column+=1;
+  this.position.column += 1;
 };
 
 Ship.prototype.moveLeft = function(){
-  this.position.row+=1;
+  this.position.row += 1;
 };
+
+Ship
 /*******************************************
 ALIEN
 *******************************************/
-function Alien(points,life){
-  this.life=life;
-  this.points=points;
+function Alien(points,life,positionX, positionY){
+  this.life = life;
+  this.points = points;
   this.position = {
-    row : 1,
-    column :1
+    row : positionY,
+    column :positionX
   };
 }
+
+//function que resta vida al alien
+Alien.prototype.removeLife = function(){
+  this.life -= 1;
+};
+
+//funcion que mueve al alien derecha
+Alien.prototype.moveRight = function(){
+  this.position.row += 1;
+};
+//funcion mover alien izquierda
+Alien.prototype.moveLeft = function(){
+  this.position.row -= 1;
+};
+//funcion mover alien hacia abajo
+Alien.prototype.moveDown = function(){
+  this.position.column += 1;
+};
+//funcion eliminar alien?
+
+//funcion disparo alien
+//JUNTAR LA FUNCION DE DISPARO DE ALIEN Y SHIP?
+//funcion de acierto de disparo alien
+//JUNTAR LA FUNCION DE IMPACTO PARA ALIEN Y SHIP?
+
 
 /*******************************************
 SHOOTING
 *******************************************/
-function Shooting(){
+function Shooting(positionX,positionY){
   this.position = [
-    {row: 1, column: 5},
-    {row: 1, column: 4},
-    {row: 1, column: 3},
-    {row: 1, column: 2},
-    {row: 1, column: 1}
+    {row: positionY, column: positionX + 4},
+    {row: positionY, column: positionX + 3},
+    {row: positionY, column: positionX + 2},
+    {row: positionY, column: positionX + 1},
+    {row: positionY, column: positionX}
 ];}
-//hacer que deje de moverse cuando impacte en un enemigo o cuando se salga del tablero
+//COMO HACER QUE DETECTE SI HA IMPACTADO CON UN ALIEN
+// Shooting.prototype.move = function(){
+//   while(!this.impacted(alienPosition) || this.isOutOfSpace()){
+//     this.goForward();
+//   }
+// };
+
 Shooting.prototype.goForward = function(){
   var head = this.position[0];
   this.position.unshift({
@@ -65,4 +98,17 @@ Shooting.prototype.goForward = function(){
     column: head.column
   });
   this.body.pop();
+};
+//Funcion que comprueba si el disparo ha acertado a un alien.
+/********************************************
+ELEGIR UNA FUNCION DE LAS DOS
+******************************************/
+function alienIsImpacted(shoot, alien){
+  return alien.position.row === shoot.position[0].row &&
+   alien.position.column === shoot.position[0].column;
+}
+
+Shooting.prototype.alienImpacted = function(alien){
+  return alien.position.row === this.position[0].row &&
+   alien.position.column === this.position[0].column;
 };
